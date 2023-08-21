dd978db1f0913d97125471d16cc007ebd0fc173f
woni <jessica901215@gmail.com>
2023-08-21 18:14:22 +0800

    feat: import AuthGuard

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,19 +1,21 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { ngModule } from './primeng/primeng';
-import { HttpClientModule } from '@angular/common/http';
-import { ReactiveFormsModule } from "@angular/forms";
-
+import {NgModule} from '@angular/core';
+import {CommonModule} from '@angular/common';
+import {ngModule} from './primeng/primeng';
+import {HttpClientModule} from '@angular/common/http';
+import {ReactiveFormsModule} from "@angular/forms";
+import {AuthGuard} from "./guard/auth.guard";
 
 @NgModule({
   declarations: [],
   imports: [
     CommonModule
-  ], exports: [
+  ],
+  exports: [
     ngModule,
     HttpClientModule,
     ReactiveFormsModule,
-  ]
+  ],
+  providers: [AuthGuard]
 })
 export class SharedModule {
 }
