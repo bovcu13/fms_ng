5bc45012220221fc7d24e980004281b84c315a9f
zowie <C109118211@nkust.edu.tw>
2023-08-25 18:28:26 +0800

    feat: 點擊表格跳至地點&彈跳視窗變成透明

--- src/app/shared/data/products.ts
+++ src/app/shared/data/products.ts
@@ -1,23 +1,62 @@
 export const products = [
-  {"statetime": "2022-11-25 10:00:00", "state": "green", "speed": "0", "addr": "五福一路，高雄市苓雅區"},
-  {"statetime": "2022-11-25 10:00:00", "state": "green", "speed": "0", "addr": "五福一路，高雄市苓雅區"},
-  {"statetime": "2022-11-25 10:00:00", "state": "green", "speed": "0", "addr": "五福一路，高雄市苓雅區"},
-  {"statetime": "2022-11-25 10:00:00", "state": "green", "speed": "0", "addr": "五福一路，高雄市苓雅區"},
-  {"statetime": "2022-11-25 10:00:00", "state": "green", "speed": "0", "addr": "五福一路，高雄市苓雅區"},
-  {"statetime": "2022-11-25 10:00:00", "state": "green", "speed": "0", "addr": "五福一路，高雄市苓雅區"},
-  {"statetime": "2022-11-25 10:00:00", "state": "green", "speed": "0", "addr": "五福一路，高雄市苓雅區"},
-  {"statetime": "2022-11-25 10:00:00", "state": "green", "speed": "0", "addr": "五福一路，高雄市苓雅區"},
-  {"statetime": "2022-11-25 10:00:00", "state": "green", "speed": "0", "addr": "五福一路，高雄市苓雅區"},
-  {"statetime": "2022-11-25 10:00:00", "state": "green", "speed": "0", "addr": "五福一路，高雄市苓雅區"},
-  {"statetime": "2022-11-25 10:00:00", "state": "green", "speed": "0", "addr": "五福一路，高雄市苓雅區"},
-  {"statetime": "2022-11-25 10:00:00", "state": "green", "speed": "0", "addr": "五福一路，高雄市苓雅區"},
-  {"statetime": "2022-11-25 10:00:00", "state": "green", "speed": "0", "addr": "五福一路，高雄市苓雅區"},
-  {"statetime": "2022-11-25 10:00:00", "state": "green", "speed": "0", "addr": "五福一路，高雄市苓雅區"},
-  {"statetime": "2022-11-25 10:00:00", "state": "green", "speed": "0", "addr": "五福一路，高雄市苓雅區"},
-  {"statetime": "2022-11-25 10:00:00", "state": "green", "speed": "0", "addr": "五福一路，高雄市苓雅區"},
-  {"statetime": "2022-11-25 10:00:00", "state": "green", "speed": "0", "addr": "五福一路，高雄市苓雅區"},
-  {"statetime": "2022-11-25 10:00:00", "state": "green", "speed": "0", "addr": "五福一路，高雄市苓雅區"},
-  {"statetime": "2022-11-25 10:00:00", "state": "green", "speed": "0", "addr": "五福一路，高雄市苓雅區"},
-  {"statetime": "2022-11-25 10:00:00", "state": "green", "speed": "0", "addr": "五福一路，高雄市苓雅區"},
-  {"statetime": "2022-11-25 10:00:00", "state": "green", "speed": "0", "addr": "五福一路，高雄市苓雅區"}
+  {
+    "statetime": "2022-11-25 10:00:00",
+    "state": "green",
+    "speed": "10",
+    "addr": "明德路, 台北市北投區",
+    "position": {"lat": 25.11450302362639, "lng": 121.5222738032652},
+    "label": {
+      "color": "red",
+      "text": "一萱博物館"
+    },
+    "url": "assets/image/warehouse.png",
+  },
+  {
+    "statetime": "2022-11-25 12:30:00",
+    "state": "red",
+    "speed": "20",
+    "addr": "中山路，台北市中正區",
+    "position": {"lat": 25.036941, "lng": 121.518108},
+    "label": {
+      "color": "red",
+      "text": "台北101大樓"
+    },
+    "url": "assets/image/warehouse.png",
+  },
+  {
+    "statetime": "2022-11-25 15:45:00",
+    "state": "gray",
+    "speed": "5",
+    "addr": "信義路，台北市信義區",
+    "position": {"lat": 25.033964, "lng": 121.562874},
+    "label": {
+      "color": "red",
+      "text": "象山捷運站"
+    },
+    "url": "assets/image/warehouse.png",
+  },
+  {
+    "statetime": "2022-11-25 18:20:00",
+    "state": "green",
+    "speed": "15",
+    "addr": "光復南路，台北市大安區",
+    "position": {"lat": 25.026852, "lng": 121.537694},
+    "label": {
+      "color": "red",
+      "text": "國立中興大學"
+    },
+    "url": "assets/image/car1.png",
+  },
+  {
+    "statetime": "2022-11-25 21:10:00",
+    "state": "red",
+    "speed": "25",
+    "addr": "忠孝東路，台北市大安區",
+    "position": {"lat": 25.040824, "lng": 121.556745},
+    "label": {
+      "color": "red",
+      "text": "圓環"
+    },
+    "url": "assets/image/car2.png",
+  }
 ]
