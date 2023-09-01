77c7e6f93e533354e2786e7cb27cc33889dd3277
一萱 <jjps30516@gmail.com>
2023-09-01 17:42:27 +0800

    feat: 建立status資料和開合

---
+++ src/app/shared/data/status.ts
@@ -0,0 +1,132 @@
+export const status = [
+  {
+    "plate": "ABC123",
+    "driver": "張三",
+    "start_time": "2023-09-01 08:00:00",
+    "end_time": "2023-09-01 10:00:00",
+    "mileage": 50.5, //行駛里程
+    "usage_time": "2小時", //用車時間
+    "driving_time": "1.5小時", //開車時間
+    "idle_time": "0.5小時", //停不熄火時間
+    "over_speed_time": "0小時", //超速時間
+    "engine_off_time": "0小時", //熄火時間
+    "continuous": 1 //連續用車次數
+  },
+  {
+    "plate": "XYZ456",
+    "driver": "李四",
+    "start_time": "2023-09-01 12:00:00",
+    "end_time": "2023-09-01 14:30:00",
+    "mileage": 70.2,
+    "usage_time": "2.5小時",
+    "driving_time": "2小時",
+    "idle_time": "0.5小時",
+    "over_speed_time": "0.5小時",
+    "engine_off_time": "0小時",
+    "continuous": 1
+  },
+  {
+    "plate": "DEF789",
+    "driver": "王五",
+    "start_time": "2023-09-02 09:15:00",
+    "end_time": "2023-09-02 11:30:00",
+    "mileage": 40.8,
+    "usage_time": "2小時15分",
+    "driving_time": "1.75小時",
+    "idle_time": "0.5小時",
+    "over_speed_time": "0小時",
+    "engine_off_time": "0小時",
+    "continuous": 1
+  },
+  {
+    "plate": "GHI101",
+    "driver": "陳六",
+    "start_time": "2023-09-02 14:00:00",
+    "end_time": "2023-09-02 16:45:00",
+    "mileage": 85.3,
+    "usage_time": "2小時45分",
+    "driving_time": "2小時",
+    "idle_time": "0.75小時",
+    "over_speed_time": "0.25小時",
+    "engine_off_time": "0小時",
+    "continuous": 1
+  },
+  {
+    "plate": "JKL111",
+    "driver": "吳七",
+    "start_time": "2023-09-03 07:30:00",
+    "end_time": "2023-09-03 09:45:00",
+    "mileage": 55.6,
+    "usage_time": "2小時15分",
+    "driving_time": "1.75小時",
+    "idle_time": "0.5小時",
+    "over_speed_time": "0小時",
+    "engine_off_time": "0小時",
+    "continuous": 1
+  },
+  {
+    "plate": "MNO222",
+    "driver": "劉八",
+    "start_time": "2023-09-03 12:15:00",
+    "end_time": "2023-09-03 14:30:00",
+    "mileage": 63.7,
+    "usage_time": "2小時15分",
+    "driving_time": "1.75小時",
+    "idle_time": "0.5小時",
+    "over_speed_time": "0小時",
+    "engine_off_time": "0小時",
+    "continuous": 1
+  },
+  {
+    "plate": "PQR333",
+    "driver": "林九",
+    "start_time": "2023-09-04 08:30:00",
+    "end_time": "2023-09-04 10:45:00",
+    "mileage": 47.2,
+    "usage_time": "2小時15分",
+    "driving_time": "1.75小時",
+    "idle_time": "0.5小時",
+    "over_speed_time": "0小時",
+    "engine_off_time": "0小時",
+    "continuous": 1
+  },
+  {
+    "plate": "STU444",
+    "driver": "張十",
+    "start_time": "2023-09-04 14:00:00",
+    "end_time": "2023-09-04 16:15:00",
+    "mileage": 58.9,
+    "usage_time": "2小時15分",
+    "driving_time": "1.75小時",
+    "idle_time": "0.5小時",
+    "over_speed_time": "0小時",
+    "engine_off_time": "0小時",
+    "continuous": 1
+  },
+  {
+    "plate": "VWX555",
+    "driver": "許十一",
+    "start_time": "2023-09-05 07:45:00",
+    "end_time": "2023-09-05 10:00:00",
+    "mileage": 42.4,
+    "usage_time": "2小時15分",
+    "driving_time": "1.75小時",
+    "idle_time": "0.5小時",
+    "over_speed_time": "0小時",
+    "engine_off_time": "0小時",
+    "continuous": 1
+  },
+  {
+    "plate": "YZA666",
+    "driver": "張十二",
+    "start_time": "2023-09-05 12:00:00",
+    "end_time": "2023-09-05 14:15:00",
+    "mileage": 53.1,
+    "usage_time": "2小時15分",
+    "driving_time": "1.75小時",
+    "idle_time": "0.5小時",
+    "over_speed_time": "0小時",
+    "engine_off_time": "0小時",
+    "continuous": 1
+  }
+]
--- src/app/views/features/driving-status/driving-status.component.ts
+++ src/app/views/features/driving-status/driving-status.component.ts
@@ -1,10 +1,26 @@
 import { Component } from '@angular/core';
+import { status } from "../../../shared/data/status";
 
 @Component({
   selector: 'app-driving-status',
   templateUrl: './driving-status.component.html',
-  styleUrls: ['./driving-status.component.css']
+  styleUrls: ['./driving-status.component.scss']
 })
 export class DrivingStatusComponent {
+  status: any[] = status;
+  selectedStatus: any;
 
+  sidebarRightOpen = true;
+
+  toggleSidebar() {
+    this.sidebarRightOpen = !this.sidebarRightOpen;
+  }
+
+  getMiddleDivClass() {
+    if (this.sidebarRightOpen) {
+      return 'col-12 md:col-12 lg:col-5 ml-3';
+    } else {
+      return 'col-12 md:col-12 lg:col-12';
+    }
+  }
 }
