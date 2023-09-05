70db8e181c4213ba715c34219b7203695051c20e
一萱 <jjps30516@gmail.com>
2023-09-05 17:35:51 +0800

    test: 加入DrawingManager，可自行拉取圖形

--- src/app/views/features/test/draw/draw.component.ts
+++ src/app/views/features/test/draw/draw.component.ts
@@ -1,10 +1,57 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { HttpClient } from "@angular/common/http";
+import { Observable } from "rxjs";
+import { GoogleMap } from "@angular/google-maps";
+import { map, catchError } from 'rxjs/operators';
+import { of } from 'rxjs';
+
+declare var google: any;
 
 @Component({
   selector: 'app-draw',
   templateUrl: './draw.component.html',
   styleUrls: ['./draw.component.scss']
 })
-export class DrawComponent {
+export class DrawComponent implements OnInit {
 
+  @ViewChild(GoogleMap, { static: false }) set map(m: GoogleMap) {
+    if (m) {
+      this.initDrawingManager(m);
+    }
+  }
+
+  apiLoaded: Observable<boolean>;
+  drawingManager: any;
+
+  options: google.maps.MapOptions = {
+    center: { lat: 64.79728743642762, lng: -150.995535452303 },
+    zoom: 7,
+  };
+
+  ngOnInit(): void { }
+  constructor(httpClient: HttpClient) {
+    this.apiLoaded = httpClient.jsonp('https://maps.googleapis.com/maps/api/js?key=AIzaSyB1hde-5CDelK8n5aMiRecPOcl4i_nx0EE&libraries=drawing', 'callback')
+                               .pipe(
+                                 map(() => true),
+                                 catchError(() => of(false)),
+                               );
+  }
+
+  initDrawingManager(map: GoogleMap) {
+    const drawingOptions: google.maps.drawing.DrawingManagerOptions = {
+      drawingMode: google.maps.drawing.OverlayType.POLYGON,
+      drawingControl: true,
+      drawingControlOptions: {
+        position: google.maps.ControlPosition.TOP_CENTER,
+        drawingModes: [
+          google.maps.drawing.OverlayType.POLYGON,
+        ],
+      },
+      polygonOptions: {
+        strokeColor: '#00ff00',
+      },
+    };
+    this.drawingManager = new google.maps.drawing.DrawingManager(drawingOptions);
+    this.drawingManager.setMap(map.googleMap);
+  }
 }
