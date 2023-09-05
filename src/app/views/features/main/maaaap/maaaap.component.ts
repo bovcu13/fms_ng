27a4b0c49c7567c891bc23f2c73820ff364d37c4
一萱 <jjps30516@gmail.com>
2023-09-05 15:02:15 +0800

    test: 繪製可編輯的多邊形

--- src/app/views/features/main/maaaap/maaaap.component.ts
+++ src/app/views/features/main/maaaap/maaaap.component.ts
@@ -1,43 +1,90 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
+declare var google: any;
 
 @Component({
   selector: 'app-maaaap',
   templateUrl: './maaaap.component.html',
   styleUrls: ['./maaaap.component.scss']
 })
-export class MaaaapComponent {
-  polyPath: google.maps.LatLngLiteral[] = [
-    { lat: 25.03280092118552, lng: 121.56348748779168 },
-    { lat: 25.03587797931996, lng: 121.56351157458673 },
-    { lat: 25.03583432131525, lng: 121.56543846794476 },
-    { lat: 25.033019138809674, lng: 121.56546250540032 },
-    { lat: 25.033062791203154, lng: 121.56201826717597 },
-  ];
-  center: google.maps.LatLngLiteral = {
-    lat: 25.0336962,
-    lng: 121.5643673,
-  };
-  polyOptions: google.maps.PolylineOptions = {
-    strokeColor: '#40809d',
-    strokeOpacity: 1,
-    strokeWeight: 10,
-    icons: [
-      {
-        icon: {
-          path: google.maps.SymbolPath.FORWARD_CLOSED_ARROW,
-        },
-        offset: '100%',
-      },
-    ],
-  };
-  //
-  zoom = 17;
-  options: google.maps.MapOptions = {
-    disableDefaultUI: true,
-    backgroundColor: '#126df5',
-    clickableIcons: false,
-    disableDoubleClickZoom: true,
-    draggable: true,
-    zoomControl: true,
-  };
+export class MaaaapComponent implements OnInit {
+  map: any;
+
+  ngOnInit() {
+    this.map = new google.maps.Map(document.getElementById('map'), {
+      center: { lat: 64.79728743642762, lng: -150.995535452303 },
+      zoom: 7
+    });
+
+    // 创建多边形
+    let polygon = new google.maps.Polygon({
+      map: this.map,
+      paths: [
+        // 多邊形座標
+        [
+          {
+            lat: 65.14126095716469,
+            lng: -155.510916311678
+          },
+          {
+            lat: 65.27485886465622,
+            lng: -154.642996389803
+          },
+          {
+            lat: 65.63992346657838,
+            lng: -154.500174124178
+          },
+          {
+            lat: 65.80704912165639,
+            lng: -155.159353811678
+          },
+          {
+            lat: 65.64898494006725,
+            lng: -156.093191702303
+          },
+          {
+            lat: 65.27026326043044,
+            lng: -156.356863577303
+          }
+        ],
+        [
+          {
+            lat: 65.30241568666297,
+            lng: -151.522879202303
+          },
+          {
+            lat: 65.48539327007941,
+            lng: -149.984793264803
+          },
+          {
+            lat: 65.73491119176467,
+            lng: -150.171560842928
+          },
+          {
+            lat: 65.91039432417611,
+            lng: -150.819754202303
+          },
+          {
+            lat: 65.5400380950869,
+            lng: -152.116140921053
+          },
+          {
+            lat: 65.37118184344384,
+            lng: -152.138113577303
+          }
+        ]
+      ],
+      strokeColor: '#FF8C00',
+      fillColor: '#FF8C00',
+      strokeOpacity: 0.8,
+      strokeWeight: 2,
+      editable: true
+    });
+    // 事件監聽器
+    google.maps.event.addListener(polygon, 'click', function () {
+      // 點擊多邊形時的處理
+      polygon.setOptions({ fillColor: '#000' });
+      polygon.setOptions({ strokeColor: '#000' });
+    });
+  }
 }
