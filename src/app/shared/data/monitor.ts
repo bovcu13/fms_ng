77429a0c8bc82022396a78fd2e91152c09f39b5c
woni <jessica901215@gmail.com>
2023-09-06 10:03:28 +0800

    feat: geocode & 註解

--- src/app/shared/data/monitor.ts
+++ src/app/shared/data/monitor.ts
@@ -5,8 +5,17 @@ export const monitorStatus: any = [
         driverName: 'John Smith',
         vehicleName: '卡車 A',
         camera: '前置攝像頭',
-        location: '廣州，天河區',
+        addr: '廣州，天河區',
         speed: 80,
+        position: {
+            lat: 25.11450302362639,
+            lng: 121.5222738032652
+        },
+        label: {
+            color: "red",
+            "text": "一萱博物館"
+        },
+        url: "assets/image/warehouse.png",
     },
     {
         status: '停車中',
@@ -14,8 +23,17 @@ export const monitorStatus: any = [
         driverName: 'Alice Johnson',
         vehicleName: '車輛 B',
         camera: '後置攝像頭',
-        location: '深圳，南山區',
+        addr: '深圳，南山區',
         speed: 0,
+        position: {
+            lat: 25.036941,
+            lng: 121.518108
+        },
+        label: {
+            color: "red",
+            text: "台北101大樓"
+        },
+        url: "assets/image/warehouse.png",
     },
     {
         status: '運行中',
@@ -23,8 +41,17 @@ export const monitorStatus: any = [
         driverName: 'Bob Brown',
         vehicleName: 'SUV',
         camera: '360度攝像頭',
-        location: '珠海，香洲區',
+        addr: '珠海，香洲區',
         speed: 65,
+        position: {
+            lat: 25.033964,
+            lng: 121.562874
+        },
+        label: {
+            color: "red",
+            text: "象山捷運站"
+        },
+        url: "assets/image/warehouse.png",
     },
     {
         status: '運行中',
@@ -32,8 +59,17 @@ export const monitorStatus: any = [
         driverName: 'Bob Brown',
         vehicleName: 'SUV',
         camera: '360度攝像頭',
-        location: '珠海，香洲區',
+        addr: '珠海，香洲區',
         speed: 65,
+        position: {
+            lat: 25.026852,
+            lng: 121.537694
+        },
+        label: {
+            color: "red",
+            text: "國立中興大學"
+        },
+        url: "assets/image/car1.png",
     },
     {
         status: '運行中',
@@ -41,8 +77,17 @@ export const monitorStatus: any = [
         driverName: 'Bob Brown',
         vehicleName: 'SUV',
         camera: '360度攝像頭',
-        location: '珠海，香洲區',
+        addr: '珠海，香洲區',
         speed: 65,
+        position: {
+            lat: 25.040824,
+            lng: 121.556745
+        },
+        label: {
+            color: "red",
+            text: "圓環"
+        },
+        url: "assets/image/car2.png",
     },
     {
         status: '運行中',
@@ -50,8 +95,17 @@ export const monitorStatus: any = [
         driverName: 'Bob Brown',
         vehicleName: 'SUV',
         camera: '360度攝像頭',
-        location: '珠海，香洲區',
+        addr: '珠海，香洲區',
         speed: 65,
+        position: {
+            lat: 25.026852,
+            lng: 121.537694
+        },
+        label: {
+            color: "red",
+            text: "國立中興大學"
+        },
+        url: "assets/image/car1.png",
     },
     {
         status: '運行中',
@@ -59,7 +113,16 @@ export const monitorStatus: any = [
         driverName: 'Bob Brown',
         vehicleName: 'SUV',
         camera: '360度攝像頭',
-        location: '珠海，香洲區',
+        addr: '珠海，香洲區',
         speed: 65,
+        position: {
+            lat: 25.026852,
+            lng: 121.537694
+        },
+        label: {
+            color: "red",
+            text: "國立中興大學"
+        },
+        url: "assets/image/car1.png",
     },
 ];
--- src/app/views/features/monitor/monitor.component.ts
+++ src/app/views/features/monitor/monitor.component.ts
@@ -1,197 +1,210 @@
-import {Component, OnDestroy, OnInit, ViewChild} from '@angular/core';
-import {products} from "../../../shared/data/products";
+import {Component, OnInit, ViewChild} from '@angular/core';
 import {MapInfoWindow, MapMarker} from "@angular/google-maps";
-import {interval, Subscription} from 'rxjs';
 import {monitorStatus} from "../../../shared/data/monitor";
 
 @Component({
-    selector: 'app-monitor',
-    templateUrl: './monitor.component.html',
-    styleUrls: ['./monitor.component.scss']
+  selector: 'app-monitor',
+  templateUrl: './monitor.component.html',
+  styleUrls: ['./monitor.component.scss']
 })
-export class MonitorComponent implements OnInit, OnDestroy {
-    videoItems = [
-        {
-            name: 'Video one',
-            src: 'assets/vdo/swimming.mp4',
-            type: 'video/mp4'
-        }
-    ];
-    sidebarRightOpen = true;
-    mapOpen = true;
-    activeIndex = 0;
-    currentVideo = this.videoItems[this.activeIndex];
-    data: any;
-    totalItems: number = 8;
-    rows: number = Math.ceil(Math.sqrt(this.totalItems)) + 1;
-    cols: number = Math.ceil(this.totalItems / this.rows);
-    layout: number[] = [];
-    value: number = 4;
-    height: string = 'h1-2'
-    selectedCar: any = [];
-    scrollHeight: string = '350px';
-    markers: any[] = [];
-    paymentOptions: any[] = [
-        {name: '1', value: 0, row: 1, height: 'h1-1'},
-        {name: '2', value: 1, row: 2, height: 'h1-1'},
-        {name: '4', value: 2, row: 2, height: 'h1-2'},
-        {name: '6', value: 3, row: 3, height: 'h1-2'},
-        {name: '8', value: 4, row: 4, height: 'h1-2'},
-        {name: '12', value: 5, row: 4, height: 'h1-3'},
-        {name: '16', value: 6, row: 4, height: 'h1-4'}
-    ];
-
-    carGroups: any = [
-        {name: '車隊(A)', code: 'A'},
-        {name: '車隊(B)', code: 'B'},
-        {name: '車隊(C)', code: 'C'},
-        {name: '車隊(D)', code: 'D'},
-    ]
-
-    monitorStatus: any = monitorStatus;
-
-    @ViewChild(MapInfoWindow, {static: false}) info!: MapInfoWindow
-    @ViewChild(MapInfoWindow) infoWindow!: MapInfoWindow;
-    @ViewChild('map', {static: false}) map: any;
-    products: any[] = products;
-    selectedProduct: any;
-
-    // google map
-    polyPath: google.maps.LatLngLiteral[] = [
-        {lat: 25.03280092118552, lng: 121.56348748779168},
-        {lat: 25.03587797931996, lng: 121.56351157458673},
-        {lat: 25.03583432131525, lng: 121.56543846794476},
-        {lat: 25.033019138809674, lng: 121.56546250540032},
-        {lat: 25.033062791203154, lng: 121.56201826717597},
-    ];
-    center: google.maps.LatLngLiteral = {
-        lat: 25.0336962,
-        lng: 121.5643673,
-    };
-    zoom = 17;
-    options: google.maps.MapOptions = {
-        disableDefaultUI: true,
-        backgroundColor: '#126df5',
-        clickableIcons: false,
-        disableDoubleClickZoom: true,
-        draggable: true,
-        zoomControl: true,
-    };
-
-    constructor() {
-        console.log('每列 ' + this.rows + ' 個\n' + '共 ' + this.cols + ' 列')
-        for (let i = 0; i < this.totalItems; i++) {
-            this.layout.push(i);
-        }
-        console.log('layout ' + this.layout)
+export class MonitorComponent implements OnInit {
+  videoItems = [
+    {
+      name: 'Video one',
+      src: 'assets/vdo/swimming.mp4',
+      type: 'video/mp4'
     }
+  ];
+  monitorStatus: any = monitorStatus;
+  sidebarRightOpen = true;
+  mapOpen = true;
+  data: any;
+  totalItems: number = 8;
+  rows: number = Math.ceil(Math.sqrt(this.totalItems)) + 1;
+  cols: number = Math.ceil(this.totalItems / this.rows);
+  layout: number[] = [];
+  value: number = 4;
+  height: string = 'h1-2'
+  selectedCar: any = [];
+  scrollHeight: string = '350px';
+  markers: any[] = [];
 
-    ngOnDestroy(): void {
-        throw new Error('Method not implemented.');
+  // 調整視窗顯示排版 & 高度
+  paymentOptions: any[] = [
+    {name: '1', value: 0, row: 1, height: 'h1-1'},
+    {name: '2', value: 1, row: 2, height: 'h1-1'},
+    {name: '4', value: 2, row: 2, height: 'h1-2'},
+    {name: '6', value: 3, row: 3, height: 'h1-2'},
+    {name: '8', value: 4, row: 4, height: 'h1-2'},
+    {name: '12', value: 5, row: 4, height: 'h1-3'},
+    {name: '16', value: 6, row: 4, height: 'h1-4'}
+  ];
+
+  carGroups: any = [
+    {name: '車隊(A)', code: 'A'},
+    {name: '車隊(B)', code: 'B'},
+    {name: '車隊(C)', code: 'C'},
+    {name: '車隊(D)', code: 'D'},
+  ]
+
+  @ViewChild(MapInfoWindow, {static: false}) info!: MapInfoWindow
+  @ViewChild(MapInfoWindow) infoWindow!: MapInfoWindow;
+  @ViewChild('map', {static: false}) map: any;
+
+  // google map
+  polyPath: google.maps.LatLngLiteral[] = [
+    {lat: 25.03280092118552, lng: 121.56348748779168},
+    {lat: 25.03587797931996, lng: 121.56351157458673},
+    {lat: 25.03583432131525, lng: 121.56543846794476},
+    {lat: 25.033019138809674, lng: 121.56546250540032},
+    {lat: 25.033062791203154, lng: 121.56201826717597},
+  ];
+  center: google.maps.LatLngLiteral = {
+    lat: 25.0336962,
+    lng: 121.5643673,
+  };
+  zoom = 17;
+  options: google.maps.MapOptions = {
+    disableDefaultUI: true,
+    backgroundColor: '#126df5',
+    clickableIcons: false,
+    disableDoubleClickZoom: true,
+    draggable: true,
+    zoomControl: true,
+  };
+
+  constructor() {
+    console.log('每列 ' + this.rows + ' 個\n' + '共 ' + this.cols + ' 列')
+    for (let i = 0; i < this.totalItems; i++) {
+      this.layout.push(i);
     }
+    console.log('layout ' + this.layout)
+  }
 
-    ngOnInit(): void {
-        console.log(this.totalItems)
-        this.markers = products.map(product => ({
-            position: product.position,
-            icon: {url: product.url, scaledSize: new google.maps.Size(50, 50)},
-            label: {text: product.label.text},
-            infoWindowContent: product.label.text
-        }));
+  ngOnInit(): void {
+    console.log(this.totalItems)
+    this.geocodePositions();
+    this.markers = monitorStatus.map((car: { position: any; url: any; label: { text: any; }; }) => ({
+      position: car.position,
+      icon: {url: car.url, scaledSize: new google.maps.Size(50, 50)},
+      label: {text: car.label.text},
+      infoWindowContent: car.label.text
+    }));
+  }
+
+  // 點擊表格內容，地圖跳轉到該地點
+  Select() {
+    if (this.selectedCar) {
+      this.center = this.selectedCar.position;
     }
+  }
 
-    videoPlayerInit(data: any) {
-        this.data = data;
-        this.data.getDefaultMedia().subscriptions.loadedMetadata.subscribe(this.initVdo.bind(this));
-        this.data.getDefaultMedia().subscriptions.ended.subscribe(this.nextVideo.bind(this));
+  // count() {
+  //   this.rows = Math.ceil(Math.sqrt(this.totalItems));
+  //   this.cols = Math.ceil(this.totalItems / this.rows);
+  //   console.log('每列 ' + this.rows + ' 個\n' + '共 ' + this.cols + ' 列')
+  // }
+
+  // 點擊 Add
+  plus() {
+    if (this.totalItems < 16) {
+      this.totalItems++;
+      this.layout.push(this.totalItems - 1);
     }
+    console.log(this.totalItems);
+    console.log('layout ' + this.layout);
+    // this.count();
+  }
 
-    nextVideo() {
-        this.activeIndex++;
-        if (this.activeIndex === this.videoItems.length) {
-            this.activeIndex = 0;
-        }
-        this.currentVideo = this.videoItems[this.activeIndex];
+  // 點擊 Remove
+  minus() {
+    if (this.totalItems > 0) {
+      this.totalItems--;
+      this.layout.pop();
     }
+    console.log(this.totalItems)
+    console.log('layout ' + this.layout)
+    // this.count();
+  }
 
-    initVdo() {
-        this.data.play();
+  // 選擇 selectButton
+  changeRow() {
+    this.height = this.paymentOptions[this.value].height;
+    this.rows = this.paymentOptions[this.value].row;
+    this.cols = Math.ceil(this.totalItems / this.rows);
+    console.log(' col: ' + this.cols + '\n row: ' + this.rows + '\n height: ' + this.height)
+  }
+
+  // 開合側邊欄按鈕
+  toggleSidebar() {
+    this.sidebarRightOpen = !this.sidebarRightOpen;
+  }
+
+  // 開合地圖按鈕
+  toggleMap() {
+    this.mapOpen = !this.mapOpen;
+    if (this.mapOpen) {
+      this.scrollHeight = '350px'
+    } else {
+      this.scrollHeight = '520px'
     }
+  }
 
-    startPlaylistVdo(item: any, index: number) {
-        this.activeIndex = index;
-        this.currentVideo = item;
+  // 配合側邊欄開關調整寬度
+  getMiddleDivClass() {
+    if (this.sidebarRightOpen) {
+      return 'col-12 md:col-12 lg:col-8';
+    } else {
+      return 'col-12 md:col-12 lg:col-12';
     }
+  }
 
-    // count() {
-    //   this.rows = Math.ceil(Math.sqrt(this.totalItems));
-    //   this.cols = Math.ceil(this.totalItems / this.rows);
-    //   console.log('每列 ' + this.rows + ' 個\n' + '共 ' + this.cols + ' 列')
-    // }
+  // 點擊地圖會在中間
+  moveMap(event: google.maps.MapMouseEvent) {
+    if (event.latLng != null) this.center = (event.latLng.toJSON());
+  }
 
-    plus() {
-        if (this.totalItems < 16) {
-            this.totalItems++;
-            this.layout.push(this.totalItems - 1);
-        }
-        console.log(this.totalItems);
-        console.log('layout ' + this.layout);
-        // this.count();
-    }
+  display: any
 
-    minus() {
-        if (this.totalItems > 0) {
-            this.totalItems--;
-            this.layout.pop();
-        }
-        console.log(this.totalItems)
-        console.log('layout ' + this.layout)
-        // this.count();
-    }
+  move(event: google.maps.MapMouseEvent) {
+    if (event.latLng != null) this.display = event.latLng.toJSON();
+  }
 
-    changeRow() {
-        this.height = this.paymentOptions[this.value].height;
-        this.rows = this.paymentOptions[this.value].row;
-        this.cols = Math.ceil(this.totalItems / this.rows);
-        console.log(' col: ' + this.cols + '\n row: ' + this.rows + '\n height: ' + this.height)
-    }
+  infoContent: any = ''
 
-    toggleSidebar() {
-        this.sidebarRightOpen = !this.sidebarRightOpen;
-    }
+  // 開啟標記標籤的內容
+  openInfo(marker: MapMarker, content: string) {
+    this.infoContent = content;
+    this.info.open(marker)
+  }
 
-    toggleMap() {
-        this.mapOpen = !this.mapOpen;
-        if (this.mapOpen) {
-            this.scrollHeight = '350px'
+  // 將座標轉為地址
+  geocodePositions() {
+    const geocoder = new google.maps.Geocoder();
+
+    this.monitorStatus.forEach((car: { position: { lat: number | google.maps.LatLngLiteral | google.maps.LatLng; lng: number | boolean | null | undefined; }; addr: string; }) => {
+      const latlng = new google.maps.LatLng(car.position.lat, car.position.lng);
+
+      geocoder.geocode({location: latlng}, (results, status) => {
+        if (status === google.maps.GeocoderStatus.OK) {
+          let addressFound = false;
+          if (results && results.length > 0) {
+            for (let i = 0; i < results.length; i++) {
+              const formattedAddress = results[i].formatted_address;
+              if (!formattedAddress.match(/\b\w+\+\w+\b/)) {
+                car.addr = formattedAddress;
+                addressFound = true;
+                break; // 找到非 Plus Code 地址後跳出迴圈
+              }
+            }
+          }
+          if (!addressFound) {
+            car.addr = '找不到地址';
+          }
         } else {
-            this.scrollHeight = '520px'
+          car.addr = '編碼錯誤';
         }
-    }
-
-    getMiddleDivClass() {
-        if (this.sidebarRightOpen) {
-            return 'col-12 md:col-12 lg:col-8';
-        } else {
-            return 'col-12 md:col-12 lg:col-12';
-        }
-    }
-
-    //點擊地圖會在中間
-    moveMap(event: google.maps.MapMouseEvent) {
-        if (event.latLng != null) this.center = (event.latLng.toJSON());
-    }
-
-    display: any
-
-    move(event: google.maps.MapMouseEvent) {
-        if (event.latLng != null) this.display = event.latLng.toJSON();
-    }
-
-    infoContent: any = ''
-    //開啟標記標籤的內容
-    openInfo(marker: MapMarker, content: string) {
-        this.infoContent = content;
-        this.info.open(marker)
-    }
+      });
+    });
+  }
 }
