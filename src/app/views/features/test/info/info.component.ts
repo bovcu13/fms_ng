331f2c4e0572861452c8e7d646afc7bbd90f68c1
一萱 <jjps30516@gmail.com>
2023-09-06 13:48:34 +0800

    test: 還原main原來的功能

--- src/app/views/features/test/info/info.component.ts
+++ src/app/views/features/test/info/info.component.ts
@@ -24,18 +24,7 @@ export class InfoComponent implements OnInit {
     mapTypeId: google.maps.MapTypeId.ROADMAP,
     maxZoom: 18,
   }
-  zoom = 15;
 
-  //點擊地圖會在中間
-  moveMap(event: google.maps.MapMouseEvent) {
-    if (event.latLng != null) this.center = (event.latLng.toJSON());
-  }
-
-  display: any
-
-  move(event: google.maps.MapMouseEvent) {
-    if (event.latLng != null) this.display = event.latLng.toJSON();
-  }
   // 創建標記
   markers: any[] = []
 
@@ -72,20 +61,55 @@ export class InfoComponent implements OnInit {
         map: map,
         title: location.addr,
         icon: { url: location.url, scaledSize: new google.maps.Size(50, 50) },
+        // options: { animation: google.maps.Animation.BOUNCE },
       });
 
       const infowindow = new google.maps.InfoWindow({
         content: location.infoWindowContent
       });
 
+      //開info
       google.maps.event.addListener(marker, 'click', () => {
         infowindow.open(map, marker);
       });
+
+      //點擊會到中心
+      marker.addListener("click", () => {
+        map.setZoom(14);
+        map.setCenter(marker.getPosition() as google.maps.LatLng);
+      });
+
+      //可以按兩下
+      map.addListener("click", (e:any) => {
+        this.placeMarkerAndPanTo(e.latLng, map);
+      });
+
 
       // 一開始就顯示資訊窗口
       infowindow.open(map, marker);
 
       this.markers.push(marker);
     }
+  }
+
+  private previousMarker: google.maps.Marker | null = null;
+
+  placeMarkerAndPanTo(latLng: google.maps.LatLng, map: google.maps.Map) {
+    // 清除之前的標記
+    if (this.previousMarker) {
+      this.previousMarker.setMap(null);
+    }
+
+    // 創建新的標記
+    const marker = new google.maps.Marker({
+      position: latLng,
+      map: map,
+    });
+
+    // 設定地圖中心為新位置
+    map.panTo(latLng);
+
+    // 將新標記設為上一個標記
+    this.previousMarker = marker;
   }
 }
