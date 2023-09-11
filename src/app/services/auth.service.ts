95a8ea0495d6d33cbf97b70db75c5c01c4bfc109
woni <jessica901215@gmail.com>
2023-09-11 14:46:33 +0800

    feat: api login signOut register function

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,4 +1,12 @@
 import {Injectable} from '@angular/core';
+import {HttpClient, HttpHeaders} from '@angular/common/http';
+import {Observable} from 'rxjs';
+
+const AUTH_API = 'https://fmp.t.api.jinher-net.com/fms';
+
+const httpOptions = {
+  headers: new HttpHeaders({'Content-Type': 'application/json'})
+};
 
 @Injectable({
   providedIn: 'root'
@@ -10,7 +18,7 @@ export class AuthService {
     {fleet_code: 'B00001', user_name: 'user1', password: '12345', role: 'user'}
   ];
 
-  constructor() {
+  constructor(private http: HttpClient) {
   }
 
   login(fleet_code: string, user_name: string, password: string): boolean {
@@ -22,8 +30,32 @@ export class AuthService {
     return false;
   }
 
+  // 登入
+  // login(body: any): Observable<any> {
+  //   const url = `${AUTH_API}/web/v1.0/login`;
+  //   return this.http.post(url, body, httpOptions);
+  // }
+
+  // 登出
+  signOut(): void {
+    window.sessionStorage.clear();
+  }
+
   logout(): void {
     localStorage.removeItem('currentUser');
+  }
+
+  register(username: string, email: string, password: string): Observable<any> {
+    return this.http.post(AUTH_API + '/web/v1.0/users', {
+      username,
+      email,
+      password
+    }, httpOptions);
+  }
+
+  refreshToken(token: string) {
+    const url = `${AUTH_API}/web/v1.0/refresh`;
+    return this.http.post(url, {refresh_token: token}, httpOptions);
   }
 
   getCurrentUser(): any {
