e3fbe7ff3e655b14e615b2255f95444cf95e4eec
woni <jessica901215@gmail.com>
2023-09-12 14:21:15 +0800

    feat: 權限

--- src/app/services/token-storage.service.ts
+++ src/app/services/token-storage.service.ts
@@ -72,9 +72,8 @@ export class TokenStorageService {
   public getUser(): any {
     const user = window.sessionStorage.getItem(USER_KEY);
     if (user) {
-      return JSON.parse(user);
+      return user;
     }
-
     return {};
   }
 }
--- src/app/shared/guard/auth.guard.ts
+++ src/app/shared/guard/auth.guard.ts
@@ -1,7 +1,6 @@
 import {ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot, UrlTree} from '@angular/router';
 import {AuthService} from "../../services/auth.service";
 import {Injectable} from '@angular/core';
-import {Observable} from 'rxjs';
 import {TokenStorageService} from "../../services/token-storage.service";
 
 @Injectable({
@@ -11,7 +10,7 @@ import {TokenStorageService} from "../../services/token-storage.service";
 export class AuthGuard implements CanActivate {
 
   constructor(
-    private token: TokenStorageService,
+    private tokenStorage: TokenStorageService,
     private authServ: AuthService,
     private router: Router
   ) {
@@ -29,15 +28,17 @@ export class AuthGuard implements CanActivate {
   //   return this.router.createUrlTree(['/login']); // Redirect to login page if not logged in
   // }
 
-  canActivate(
-    route: ActivatedRouteSnapshot,
-    state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
-
-    // access token 是否有值
-    if (this.token.getToken()) {
-      return true; // 允許訪問受保護的路由
-    } else {
-      return this.router.createUrlTree(['/login']); // 返回UrlTree以重定向到登錄頁面
+  canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): boolean | UrlTree {
+    const currentUser = this.tokenStorage.getUser();
+    if (this.tokenStorage.getToken()) {
+      if (currentUser) {
+        if (route.routeConfig?.path === 'role' && currentUser !== 'admin') {
+          console.log('沒有權限')
+          return this.router.createUrlTree(['/main']);
+        }
+        return true;
+      }
     }
+    return this.router.createUrlTree(['/login']);
   }
 }
