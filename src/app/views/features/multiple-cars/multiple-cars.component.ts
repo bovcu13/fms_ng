3c526750e8d3c965deb96530ee9d747cfa883711
一萱 <jjps30516@gmail.com>
2023-09-12 11:51:18 +0800

    test: 改成按鈕

--- src/app/views/features/multiple-cars/multiple-cars.component.ts
+++ src/app/views/features/multiple-cars/multiple-cars.component.ts
@@ -33,23 +33,40 @@ export class MultipleCarsComponent implements OnInit {
   }
 
   check(e: any) {
-    console.log(e)
+    console.log(e.index)
     this.selectPanel = e.index;
     // 將二微陣列的值呼叫到 selectedProduct
     this.selectedProduct = this.saveSelectedProduct[e.index];
     console.log(e.originalEvent.target.innerText)
+    // if (this.Panels) {
+    //   if (e.originalEvent.target.innerText == '+') {
+    //     if (this.countPanels < 11) {
+    //       this.Panels.push(this.Panels.length + 1);
+    //       this.countPanels++;
+    //       this.sidebarRightOpen = false;
+    //       console.log('Panels.length: ' + this.Panels.length + '\ncountPanels: ' + this.countPanels)
+    //     }
+    //   } else {
+    //     this.sidebarRightOpen = true;
+    //   }
+    // }
+  }
+  activeTabIndex = 0
+  addNewTab() {
+    this.selectPanel = this.selectPanel+1
     if (this.Panels) {
-      if (e.originalEvent.target.innerText == '+') {
         if (this.countPanels < 11) {
           this.Panels.push(this.Panels.length + 1);
           this.countPanels++;
           this.sidebarRightOpen = false;
           console.log('Panels.length: ' + this.Panels.length + '\ncountPanels: ' + this.countPanels)
         }
-      } else {
+       else {
         this.sidebarRightOpen = true;
       }
     }
+    this.activeTabIndex = this.countPanels - 2
+    console.log(this.activeTabIndex)
   }
 
   close(e: any) {
@@ -100,6 +117,7 @@ export class MultipleCarsComponent implements OnInit {
   }
 
   ngOnInit(): void {
+    this.activeTabIndex = 0
     this.markers = products.map(product => ({
       position: product.position,
       icon: {url: product.url, scaledSize: new google.maps.Size(50, 50)},
