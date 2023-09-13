e57a36e049ff7b32802ba109f9fe8e395830c419
一萱 <jjps30516@gmail.com>
2023-09-13 15:06:16 +0800

    feat: 測量距離座標線

--- src/app/views/features/test/draw/draw.component.ts
+++ src/app/views/features/test/draw/draw.component.ts
@@ -9,37 +9,41 @@ declare var google: any;
 })
 export class DrawComponent implements OnInit {
 
-  marker1= google.maps.Marker
-  marker2= google.maps.Marker
-  poly= google.maps.Polyline
-  geodesicPoly= google.maps.Polyline
+  poly= google.maps.Polyline;
+  map= google.maps.Map;
 
   initMap(): void {
+    this.map = new google.maps.Map(document.getElementById("map") as HTMLElement, {
+      zoom: 7,
+      center: { lat: 41.879, lng: -87.624 }, // Center the map on Chicago, USA.
+    });
 
-    const map = new google.maps.Map(
-      document.getElementById("map") as HTMLElement,
-      {
-        center: { lat: -34.397, lng: 150.644 },
-        zoom: 8,
-      }
-    );
+    this.poly = new google.maps.Polyline({
+      strokeColor: "#000000",
+      strokeOpacity: 1.0,
+      strokeWeight: 3,
+    });
+    this.poly.setMap(this.map);
+
+    // Add a listener for the click event
+    this.map.addListener("click", this.addLatLng.bind(this));
 
     const drawingManager = new google.maps.drawing.DrawingManager({
-      drawingMode: google.maps.drawing.OverlayType.MARKER,
+      // drawingMode: google.maps.drawing.OverlayType.RECTANGLE, //預設的模式
       drawingControl: true,
       drawingControlOptions: {
         position: google.maps.ControlPosition.TOP_CENTER,
         drawingModes: [
-          google.maps.drawing.OverlayType.MARKER,
+          // google.maps.drawing.OverlayType.MARKER,
           google.maps.drawing.OverlayType.CIRCLE,
           google.maps.drawing.OverlayType.POLYGON,
           google.maps.drawing.OverlayType.POLYLINE,
           google.maps.drawing.OverlayType.RECTANGLE,
         ],
       },
-      markerOptions: {
-        icon: "https://developers.google.com/maps/documentation/javascript/examples/full/images/beachflag.png",
-      },
+      // markerOptions: {
+      //   icon: "https://developers.google.com/maps/documentation/javascript/examples/full/images/beachflag.png",
+      // },
       circleOptions: {
         fillColor: "#E9CD4C",
         fillOpacity: 0.6,
@@ -50,9 +54,60 @@ export class DrawComponent implements OnInit {
       },
     });
 
-    drawingManager.setMap(map);
-
+    drawingManager.setMap(this.map);
   }
+
+  // 在 addLatLng 方法中使用箭頭函數，以確保保留正確的上下文
+  addLatLng = (event: google.maps.MapMouseEvent) => {
+    const path = this.poly.getPath();
+
+    // Because path is an MVCArray, we can simply append a new coordinate
+    // and it will automatically appear.
+    path.push(event.latLng as google.maps.LatLng);
+
+    // Add a new marker at the new plotted point on the polyline.
+    new google.maps.Marker({
+      position: event.latLng,
+      title: "#" + path.getLength(),
+      map: this.map,
+    });
+  }
+
+    // const map = new google.maps.Map(
+    //   document.getElementById("map") as HTMLElement,
+    //   {
+    //     center: { lat: -34.397, lng: 150.644 },
+    //     zoom: 8,
+    //   }
+    // );
+    //
+    // const drawingManager = new google.maps.drawing.DrawingManager({
+    //   drawingMode: google.maps.drawing.OverlayType.MARKER,
+    //   drawingControl: true,
+    //   drawingControlOptions: {
+    //     position: google.maps.ControlPosition.TOP_CENTER,
+    //     drawingModes: [
+    //       google.maps.drawing.OverlayType.MARKER,
+    //       google.maps.drawing.OverlayType.CIRCLE,
+    //       google.maps.drawing.OverlayType.POLYGON,
+    //       google.maps.drawing.OverlayType.POLYLINE,
+    //       google.maps.drawing.OverlayType.RECTANGLE,
+    //     ],
+    //   },
+    //   markerOptions: {
+    //     icon: "https://developers.google.com/maps/documentation/javascript/examples/full/images/beachflag.png",
+    //   },
+    //   circleOptions: {
+    //     fillColor: "#E9CD4C",
+    //     fillOpacity: 0.6,
+    //     strokeWeight: 1,
+    //     clickable: false,
+    //     editable: true,
+    //     zIndex: 1,
+    //   },
+    // });
+    //
+    // drawingManager.setMap(map);
 
   ngOnInit(): void {
     this.initMap()
