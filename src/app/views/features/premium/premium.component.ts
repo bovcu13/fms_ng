bebb7d9306aed7163fbc993c961a5cefe4c3db1a
woni <jessica901215@gmail.com>
2023-10-02 15:31:13 +0800

    test: newebPay

--- src/app/views/features/premium/premium.component.ts
+++ src/app/views/features/premium/premium.component.ts
@@ -1,10 +1,48 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {CarService} from "../../../services/car.service";
 
 @Component({
   selector: 'app-premium',
   templateUrl: './premium.component.html',
   styleUrls: ['./premium.component.scss']
 })
-export class PremiumComponent {
+export class PremiumComponent implements OnInit {
+  constructor(private carServ: CarService) {
+  }
 
+  ngOnInit() {
+
+  }
+
+  pay() {
+    this.postPayment(1, '測試')
+  }
+
+  postPayment(amount: number, description: string) {
+    let body = {
+      amount: amount,
+      description: description
+    }
+    this.carServ.postPayment(body).subscribe({
+      next: res => {
+        console.log(res)
+        console.log(body)
+        this.postNewebPay(res.body)
+      },
+      error: err => {
+        console.log(err)
+      }
+    })
+  }
+
+  postNewebPay(arr: any) {
+    this.carServ.postNewebPay(arr).subscribe({
+      next: res => {
+        console.log(res)
+      },
+      error: err => {
+        console.log(err)
+      }
+    })
+  }
 }
