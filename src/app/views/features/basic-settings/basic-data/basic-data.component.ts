abdad10e839c34a97390a3f3e956ee75ffcfa6ce
bovcu13 <jjps30516@gmail.com>
2023-10-03 16:29:51 +0800

    feat: profile set

--- src/app/views/features/basic-settings/basic-data/basic-data.component.ts
+++ src/app/views/features/basic-settings/basic-data/basic-data.component.ts
@@ -1,11 +1,50 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { carData } from "../../../../shared/data/products";
+import { CarService } from "../../../../services/car.service";
 
 @Component({
   selector: 'app-basic-data',
   templateUrl: './basic-data.component.html',
   styleUrls: ['./basic-data.component.scss']
 })
-export class BasicDataComponent {
-carData :any[] = carData
+export class BasicDataComponent implements OnInit {
+  carData :any[] = carData;
+
+  constructor(private carServ: CarService) {
+  }
+
+  ngOnInit() {
+    this.getAllVehiclesRequest()
+  }
+
+  cars: any;
+
+  vehiclesData: any;
+
+  // 取得車牌
+  getAllVehiclesRequest() {
+    this.carServ.getAllVehiclesRequest().subscribe({
+      next: res => {
+        this.vehiclesData = res.body.vehicles;
+        this.cars = this.vehiclesData.map((item: any) => ({
+          name: item.license_plate,
+          code: item.license_plate
+        }));
+      },
+      error: (err) => {
+        console.log(err);
+      },
+    });
+  }
+
+  editable = false;
+  edit() {
+    this.editable = true;
+  }
+  save() {
+    this.editable = false;
+  }
+  cancel() {
+    this.editable = false;
+  }
 }
