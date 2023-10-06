afc4d7366fba77f953bdfcd2f4acd08ed57c3545
bovcu13 <jjps30516@gmail.com>
2023-10-06 14:51:14 +0800

    feat: 新增公司&聯絡人

--- src/app/views/features/customer/customer/customer.component.ts
+++ src/app/views/features/customer/customer/customer.component.ts
@@ -7,4 +7,10 @@ import { Component } from '@angular/core';
 })
 export class CustomerComponent {
 
+  addCustomerVisible = false;
+
+  openAddCustomer() {
+    this.addCustomerVisible = true;
+  }
+
 }
