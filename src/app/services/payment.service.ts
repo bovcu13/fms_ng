162ec1263b9137fc0e41395eec3377fa05519f87
woni <jessica901215@gmail.com>
2023-10-13 15:13:12 +0800

    feat: test newebpay

---
+++ src/app/services/payment.service.ts
@@ -0,0 +1,42 @@
+import {Injectable} from '@angular/core';
+import {HttpClient, HttpHeaders, HttpParams} from '@angular/common/http'; //http協定
+import {Observable} from 'rxjs';
+import {environment} from "../../environments/environment.development";
+
+
+const BaseUrl: string = environment.API_URL;
+
+@Injectable({
+  providedIn: 'root'
+})
+export class PaymentService {
+
+  constructor(private http: HttpClient) { }
+
+  //--付款------------------------------------------------------------------------------------------------
+  postPayment(body: any): Observable<any> {
+    const url = `${BaseUrl}/web/v1.0/subscriptions/action-pay`;
+    return this.http.post<any>(url, body);
+  }
+
+  postRedirect(body: any): Observable<any> {
+    const url = `${BaseUrl}/web/v1.0/subscriptions/action-pay/redirect`;
+    return this.http.post<any>(url, body);
+  }
+
+  postNewebPay(arr: any): Observable<any> {
+    const body = new HttpParams()
+      .set('MerchantID', arr.MerchantID)
+      .set('TradeInfo', arr.TradeInfo)
+      .set('TradeSha', arr.TradeSha)
+      .set('Version', arr.Version)
+
+    return this.http.post('https://ccore.newebpay.com/MPG/mpg_gateway',
+      body.toString(),
+      {
+        headers: new HttpHeaders()
+          .set('Content-Type', 'application/x-www-form-urlencoded')
+      }
+    );
+  }
+}
