3913416c32db419cf689621de474c937b2963ce0
woni <jessica901215@gmail.com>
2023-10-17 18:17:40 +0800

    feat: 司機串接api

--- src/app/views/features/driver-mgmt/driver/driver.component.ts
+++ src/app/views/features/driver-mgmt/driver/driver.component.ts
@@ -2,53 +2,48 @@ import {Component, OnInit} from '@angular/core';
 import {Router} from "@angular/router";
 import {CarService} from "../../../../services/car.service";
 import {FormBuilder, FormGroup, Validators} from "@angular/forms";
+import {MessageService} from "primeng/api";
 
 @Component({
   selector: 'app-driver',
   templateUrl: './driver.component.html',
-  styleUrls: ['./driver.component.scss']
+  styleUrls: ['./driver.component.scss'],
+  providers: [MessageService]
 })
 export class DriverComponent implements OnInit {
 
   addDriver_form: FormGroup;
   addDialogVisible = false;
   cars: any;
-  vehiclesData: any;
+  driverData: any;
 
   constructor(
     private carServ: CarService,
     private fb: FormBuilder,
-    private router: Router
+    private router: Router,
+    private messageService: MessageService
   ) {
     this.addDriver_form = this.fb.group({
       employee_id: [''],
-      driver: ['', Validators.required],
-      mobile_phone: [''],
-      id_number: [''],
+      name: ['', Validators.required],
+      phone_number: [''],
+      id_card_number: [''],
       employee_number: [''],
       email: [''],
       address: [''],
-      daily_cost: [''],
-      created_at: [''],
-      created_by: [''],
-      updated_at: [''],
-      updated_by: [''],
+      daily_cost: ['', [Validators.pattern('^[0-9]*$')]],
     });
   }
 
   ngOnInit() {
-    this.getAllVehiclesRequest()
+    this.getAllDriversRequest()
   }
 
-  // 取得車牌
-  getAllVehiclesRequest() {
-    this.carServ.getAllVehiclesRequest(1, 20).subscribe({
+  getAllDriversRequest() {
+    this.carServ.getAllDriversRequest().subscribe({
       next: res => {
-        this.vehiclesData = res.body.vehicles;
-        this.cars = this.vehiclesData.map((item: any) => ({
-          name: item.license_plate,
-          code: item.license_plate
-        }));
+        this.driverData = res.body.drivers;
+        console.log('driverData',this.driverData);
       },
       error: (err) => {
         console.log(err);
@@ -56,21 +51,33 @@ export class DriverComponent implements OnInit {
     });
   }
 
-// 新增司機
-  addDriverRequest() {
-    this.addDialogVisible = false;
-    // this.carServ.postVehicleRequest(this.addDriver_form.value).subscribe({
-    //   next: res => {
-    //     this.addDialogVisible = false;
-    //     this.getAllVehiclesRequest();
-    //   },
-    //   error: (err) => {
-    //     console.log(err);
-    //   },
-    // });
+  // 新增司機
+  addDriver() {
+    this.addDriver_form.controls['daily_cost'].setValue(parseInt(this.addDriver_form.value.daily_cost, 10));
+    console.log('addDriver_form', this.addDriver_form.value)
+    this.carServ.postDriverRequest(this.addDriver_form.value).subscribe({
+      next: res => {
+        console.log('addDriver', res);
+        this.showSussess();
+        this.getAllDriversRequest();
+        this.addDialogVisible = false;
+      },
+      error: err => {
+        this.showError();
+        console.log('err', err);
+      }
+    })
   }
 
   goToDriver(id: any) {
     this.router.navigate(['/driver', id])
+  }
+
+  showSussess() {
+    this.messageService.add({severity: 'success', summary: '完成', detail: '新增成功！'});
+  }
+
+  showError() {
+    this.messageService.add({severity: 'error', summary: '錯誤', detail: '新增失敗！'});
   }
 }
--- src/app/views/features/driver-mgmt/view-driver/view-driver.component.ts
+++ src/app/views/features/driver-mgmt/view-driver/view-driver.component.ts
@@ -1,12 +1,14 @@
 import {Component, OnInit} from '@angular/core';
 import {CarService} from "../../../../services/car.service";
 import {FormBuilder, FormGroup, Validators} from "@angular/forms";
-import {ActivatedRoute} from "@angular/router";
+import {ActivatedRoute, Router} from "@angular/router";
+import {ConfirmationService, MessageService} from "primeng/api";
 
 @Component({
   selector: 'app-view-driver',
   templateUrl: './view-driver.component.html',
-  styleUrls: ['./view-driver.component.scss']
+  styleUrls: ['./view-driver.component.scss'],
+  providers: [ConfirmationService, MessageService]
 })
 export class ViewDriverComponent implements OnInit {
 
@@ -16,37 +18,90 @@ export class ViewDriverComponent implements OnInit {
   constructor(
     private carServ: CarService,
     private fb: FormBuilder,
-    private route: ActivatedRoute
+    private route: ActivatedRoute,
+    private router: Router,
+    private messageService: MessageService,
+    private confirmationService: ConfirmationService
   ) {
     this.editDriver_form = this.fb.group({
       employee_id: [''],
-      driver: ['', Validators.required],
-      mobile_phone: [''],
-      id_number: [''],
+      name: ['', Validators.required],
+      phone_number: [''],
+      id_card_number: [''],
       employee_number: [''],
       email: [''],
       address: [''],
-      daily_cost: [''],
-      created_at: [''],
-      created_by: [''],
-      updated_at: [''],
-      updated_by: [''],
+      daily_cost: ['', [Validators.pattern('^[0-9]*$')]],
     });
   }
 
   ngOnInit(): void {
     this.id = this.route.snapshot.paramMap.get('id');
-    this.getOneVehicleRequest(this.id);
+    this.getOneDriverRequest(this.id);
   }
 
-  getOneVehicleRequest(id: string) {
-    this.carServ.getOneVehicleRequest(id).subscribe({
+  getOneDriverRequest(id: string) {
+    this.carServ.getOneDriverRequest(id).subscribe({
       next: res => {
         this.editDriver_form.patchValue(res.body);
+        console.log('editDriver_form', this.editDriver_form.value);
       },
       error: (err) => {
         console.log(err);
       }
     })
+  }
+
+  editDriver() {
+    this.editDriver_form.controls['daily_cost'].setValue(parseInt(this.editDriver_form.value.daily_cost, 10));
+    console.log('editDriver_form', this.editDriver_form.value)
+    this.carServ.patchDriverRequest(this.id, this.editDriver_form.value).subscribe({
+      next: res => {
+        console.log('editDriver', res);
+        this.showSussess();
+        this.getOneDriverRequest(this.id);
+      },
+      error: (err) => {
+        console.log('err', err);
+        this.showError();
+      }
+    })
+  }
+
+  deleteDriver() {
+    console.log('open')
+    this.confirmationService.confirm({
+      message: `確定刪除${this.editDriver_form.controls['name'].value}?`,
+      header: '確定刪除?',
+      icon: 'pi pi-exclamation-triangle',
+      accept: () => {
+        this.carServ.deleteDriverRequest(this.id).subscribe({
+          next: res => {
+            console.log('deleteDriver', res);
+            this.showSussess('刪除');
+            this.router.navigate(['/driver']);
+          },
+          error: (err) => {
+            console.log('err', err);
+            this.showError('刪除');
+          }
+        })
+      },
+      reject: () => {
+        this.showCancel('刪除');
+      }
+    });
+  }
+
+  showSussess(info = '修改') {
+    this.messageService.add({severity: 'success', summary: '完成', detail: `${info}成功！`});
+  }
+
+  showError(info = '修改') {
+    this.messageService.add({severity: 'error', summary: '錯誤', detail: `${info}失敗！`});
+  }
+
+  showCancel(info: string = '修改') {
+    this.messageService.add({severity: 'warn', summary: '取消', detail: `取消${info}！`});
   }
 }
