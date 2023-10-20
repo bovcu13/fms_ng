2c222d63447e0637ea4237bcfff7a575ebe370f8
bovcu13 <jjps30516@gmail.com>
2023-10-20 13:18:10 +0800

    feat: 初始化倉儲管理

--- src/app/views/features/my-warehouse/warehouse-list/warehouse-list.component.ts
+++ src/app/views/features/my-warehouse/warehouse-list/warehouse-list.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { CarService } from "../../../../services/car.service";
+import { FormBuilder, Validators } from "@angular/forms";
+import { ActivatedRoute, Router } from "@angular/router";
 
 @Component({
   selector: 'app-warehouse-list',
@@ -6,5 +9,23 @@ import { Component } from '@angular/core';
   styleUrls: ['./warehouse-list.component.scss']
 })
 export class WarehouseListComponent {
+  warehousesData = [
+    {
+      id: 1,
+      name: 'A 倉庫',
+      manager: 'A 管理員',
+      address: '台北市中山區'
+    }
+  ]
 
+  constructor(
+    private carServ: CarService,
+    private fb: FormBuilder,
+    private route: ActivatedRoute,
+    private router: Router ) {
+  }
+
+  goToWarehouse(id: any) {
+    this.router.navigate(['/warehouse_list', id])
+  }
 }
