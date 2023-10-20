8511cf1ca65aa80f59c4fe1eac66d47ce0fbf6ae
bovcu13 <jjps30516@gmail.com>
2023-10-20 11:44:57 +0800

    feat: 初始化貨態追蹤

--- src/app/views/features/dispatch/e-tracking/e-tracking.component.ts
+++ src/app/views/features/dispatch/e-tracking/e-tracking.component.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+import { CarService } from "../../../../services/car.service";
+import { FormBuilder } from "@angular/forms";
+import { Router } from "@angular/router";
+import { list } from "../../../../shared/data/dispatch";
 
 @Component({
   selector: 'app-e-tracking',
@@ -6,5 +10,16 @@ import { Component } from '@angular/core';
   styleUrls: ['./e-tracking.component.scss']
 })
 export class ETrackingComponent {
+  list: any = list;
 
+  constructor(
+    private carServ: CarService,
+    private fb: FormBuilder,
+    private router: Router
+  ) {
+  }
+
+  goToForm(id: any) {
+    this.router.navigate(['/dispatch_list', id])
+  }
 }
