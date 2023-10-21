e4a7f75290b6ca4decf29b521fd1c19595b42269
MingZong Lyu <mingzong.lyu@gmail.com>
2023-10-21 19:15:27 +0800

    feat: add pay

--- src/app/views/features/premium/premium.component.ts
+++ src/app/views/features/premium/premium.component.ts
@@ -8,52 +8,63 @@ import {PaymentService} from "../../../services/payment.service";
   styleUrls: ['./premium.component.scss']
 })
 export class PremiumComponent implements OnInit {
+  payurl=""
+  MerchantID=""
+  TradeInfo=""
+  TradeSha=""
+  Version=""
 
-  premium_form: FormGroup;
+//  premium_form: FormGroup;
   constructor(
     private payServ: PaymentService,
     private fb: FormBuilder,
   ) {
-    this.premium_form = this.fb.group({
-      amount: [1, Validators.required],
-      description: ['測試', Validators.required],
-      email: ['']
-    })
+//    this.premium_form = this.fb.group({
+//      amount: [1, Validators.required],
+//      description: ['測試', Validators.required],
+//      email: ['']
+//    })
+    this.payurl = "https://ccore.newebpay.com/MPG/mpg_gateway"
   }
 
   ngOnInit() {
-
-  }
-
-  pay() {
     this.postPayment(1, '測試')
   }
+
+//  pay() {
+//    this.postPayment(1, '測試')
+//  }
 
   postPayment(amount: number, description: string) {
     let body = {
       amount: amount,
       description: description
     }
-    this.payServ.postPayment(this.premium_form.value).subscribe({
+    this.payServ.postPayment(body).subscribe({
       next: res => {
-        console.log(res)
-        console.log(body)
-        this.postRedirect(res.body)
+        this.MerchantID = res.body.MerchantID
+        this.TradeInfo = res.body.TradeInfo
+        this.TradeSha = res.body.TradeSha
+        this.Version = res.body.Version
+//        console.log(res.body.MerchantID)
+//        console.log(res)
+//        console.log(body)
+//        this.postRedirect(res.body)
       },
       error: err => {
         console.log(err)
       }
     })
   }
-
-  postRedirect(arr: any) {
-    this.payServ.postRedirect(arr).subscribe({
-      next: res => {
-        console.log(res)
-      },
-      error: err => {
-        console.log(err)
-      }
-    })
-  }
+//
+//  postRedirect(arr: any) {
+//    this.payServ.postNewebPay(arr).subscribe({
+//      next: res => {
+//        console.log(res)
+//      },
+//      error: err => {
+//        console.log(err)
+//      }
+//    })
+//  }
 }
