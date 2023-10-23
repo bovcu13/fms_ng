b6626e1e2884dc80bc36502f9b27c850900f496f
bovcu13 <jjps30516@gmail.com>
2023-10-23 12:08:16 +0800

    feat: e-tracking-view-map

--- src/app/views/features/dispatch/e-tracking/e-tracking-view/e-tracking-view.component.ts
+++ src/app/views/features/dispatch/e-tracking/e-tracking-view/e-tracking-view.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { MenuItem } from "primeng/api";
 
+declare var google: any;
+
 @Component({
   selector: 'app-e-tracking-view',
   templateUrl: './e-tracking-view.component.html',
@@ -10,6 +12,7 @@ export class ETrackingViewComponent implements OnInit {
   items: MenuItem[] | undefined;
 
   ngOnInit() {
+    this.initMap();
     this.items = [
       {
         label: 'Personal',
@@ -29,4 +32,146 @@ export class ETrackingViewComponent implements OnInit {
       }
     ];
   }
+
+  map: any;
+  mapOptions: any;
+  center: google.maps.LatLngLiteral = {
+    lat: 22.61295,
+    lng: 120.3196
+  };
+
+  initMap() {
+    const styledMapType = new google.maps.StyledMapType(
+      [
+        { elementType: "geometry", stylers: [{ color: "#ebe3cd" }] },
+        { elementType: "labels.text.fill", stylers: [{ color: "#523735" }] },
+        { elementType: "labels.text.stroke", stylers: [{ color: "#f5f1e6" }] },
+        {
+          featureType: "administrative",
+          elementType: "geometry.stroke",
+          stylers: [{ color: "#c9b2a6" }],
+        },
+        {
+          featureType: "administrative.land_parcel",
+          elementType: "geometry.stroke",
+          stylers: [{ color: "#dcd2be" }],
+        },
+        {
+          featureType: "administrative.land_parcel",
+          elementType: "labels.text.fill",
+          stylers: [{ color: "#ae9e90" }],
+        },
+        {
+          featureType: "landscape.natural",
+          elementType: "geometry",
+          stylers: [{ color: "#dfd2ae" }],
+        },
+        {
+          featureType: "poi",
+          elementType: "geometry",
+          stylers: [{ color: "#dfd2ae" }],
+        },
+        {
+          featureType: "poi",
+          elementType: "labels.text.fill",
+          stylers: [{ color: "#93817c" }],
+        },
+        {
+          featureType: "poi.park",
+          elementType: "geometry.fill",
+          stylers: [{ color: "#a5b076" }],
+        },
+        {
+          featureType: "poi.park",
+          elementType: "labels.text.fill",
+          stylers: [{ color: "#447530" }],
+        },
+        {
+          featureType: "road",
+          elementType: "geometry",
+          stylers: [{ color: "#f5f1e6" }],
+        },
+        {
+          featureType: "road.arterial",
+          elementType: "geometry",
+          stylers: [{ color: "#fdfcf8" }],
+        },
+        {
+          featureType: "road.highway",
+          elementType: "geometry",
+          stylers: [{ color: "#f8c967" }],
+        },
+        {
+          featureType: "road.highway",
+          elementType: "geometry.stroke",
+          stylers: [{ color: "#e9bc62" }],
+        },
+        {
+          featureType: "road.highway.controlled_access",
+          elementType: "geometry",
+          stylers: [{ color: "#e98d58" }],
+        },
+        {
+          featureType: "road.highway.controlled_access",
+          elementType: "geometry.stroke",
+          stylers: [{ color: "#db8555" }],
+        },
+        {
+          featureType: "road.local",
+          elementType: "labels.text.fill",
+          stylers: [{ color: "#806b63" }],
+        },
+        {
+          featureType: "transit.line",
+          elementType: "geometry",
+          stylers: [{ color: "#dfd2ae" }],
+        },
+        {
+          featureType: "transit.line",
+          elementType: "labels.text.fill",
+          stylers: [{ color: "#8f7d77" }],
+        },
+        {
+          featureType: "transit.line",
+          elementType: "labels.text.stroke",
+          stylers: [{ color: "#ebe3cd" }],
+        },
+        {
+          featureType: "transit.station",
+          elementType: "geometry",
+          stylers: [{ color: "#dfd2ae" }],
+        },
+        {
+          featureType: "water",
+          elementType: "geometry.fill",
+          stylers: [{ color: "#b9d3c2" }],
+        },
+        {
+          featureType: "water",
+          elementType: "labels.text.fill",
+          stylers: [{ color: "#92998d" }],
+        },
+      ],
+      { name: "Styled Map" }
+    );
+
+    // Create a map object, and include the MapTypeId to add
+    // to the map type control.
+    const map = new google.maps.Map(
+      document.getElementById("map") as HTMLElement,
+      {
+        center: this.center,
+        zoom: 16,
+        mapTypeControlOptions: {
+          mapTypeIds: ["roadmap", "satellite", "hybrid", "terrain", "styled_map"],
+        },
+      }
+    );
+
+    //Associate the styled map with the MapTypeId and set it to display.
+    map.mapTypes.set("styled_map", styledMapType);
+    map.setMapTypeId("styled_map");
+
+  }
+
 }
