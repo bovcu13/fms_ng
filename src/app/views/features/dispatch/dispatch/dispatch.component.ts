6682b757c9ddaebc794f6ead42fec562185e386c
bovcu13 <jjps30516@gmail.com>
2023-10-27 17:32:48 +0800

    feat: dispatch page

--- src/app/views/features/dispatch/dispatch/dispatch.component.ts
+++ src/app/views/features/dispatch/dispatch/dispatch.component.ts
@@ -3,6 +3,12 @@ import { FormBuilder, FormGroup, Validators } from "@angular/forms";
 import { CarService } from "../../../../services/car.service";
 import { ActivatedRoute, Router } from "@angular/router";
 import { list } from "../../../../shared/data/dispatch";
+import { workData } from "../../../../shared/data/event";
+import { CalendarOptions } from "@fullcalendar/core";
+import interactionPlugin from "@fullcalendar/interaction";
+import dayGridPlugin from "@fullcalendar/daygrid";
+import timeGridPlugin from "@fullcalendar/timegrid";
+import listPlugin from "@fullcalendar/list";
 
 @Component({
   selector: 'app-dispatch',
@@ -13,8 +19,6 @@ export class DispatchComponent implements OnInit {
   list: any = list;
   formList: string[] = [];
 
-  addDispatchWork_form: FormGroup;
-
   workData = [
     {
       id: 1,
@@ -24,6 +28,83 @@ export class DispatchComponent implements OnInit {
       driver: '李宜蓁'
     }
   ]
+
+  showList = true;
+  showListBtn() {
+    this.showList = true;
+    console.log(this.showList);
+  }
+  closeListBtn() {
+    this.showList = false;
+    console.log(this.showList);
+  }
+
+  // full-calendar
+  dispatchEvents: any[] = workData;
+  calendarOptions: CalendarOptions = {
+    initialView: 'dayGridMonth',
+    plugins: [
+      interactionPlugin,
+      dayGridPlugin,
+      timeGridPlugin,
+      listPlugin,
+    ],
+    headerToolbar: {
+      left: 'title',
+      right: 'today prev,next',
+    },
+    editable: false,// 是否可以進行拖拽、修改
+    selectMirror: true,
+    dayMaxEvents: true,
+    handleWindowResize: true, //隨瀏覽器窗口大小變化
+    selectable: true,
+    selectOverlap: true, //日期是否可以被重複選中多次
+    weekends: true, //日曆面板中是否顯示周末,一開始為不顯示
+    dayHeaders: true,
+    events: this.dispatchEvents,
+    //點選日期開啟新增事件
+    select: () => {
+      console.log("DATE SELECTED !!!");
+      this.showAddEventDialog('add');
+    },
+    dateClick: () => {
+      console.log("DATE CLICKED !!!");
+      this.showAddEventDialog('add');
+    },
+    //點選事件開啟編輯事件
+    eventClick: (info: any) => {
+      console.log(info.event);
+      this.showAddEventDialog('editCalendar', info.event)
+    },
+  };
+
+  // dialog
+  addEventVisible: boolean = false;
+  dialogHeader!: string;
+  showEdit = true;//判斷是否dialog為新增與編輯
+  e_id: any;
+  showAddEventDialog(type: string, event ?: any) {
+    this.dialogHeader = type === 'editList' || 'editCalendar' ? '編輯任務' : '新增任務';
+    this.addEventVisible = true;
+
+    if (event) {
+      this.event_form.patchValue({
+        name: event.title,
+        start_date: new Date((event.start)),
+        end_date: type === 'editList' ? new Date((event.end)) : new Date(this.dispatchEvents[Number(event._def.publicId) - 1].end),
+      });
+      this.showEdit = true;
+    } else {
+      this.event_form.reset();
+      this.showEdit = false;
+    }
+  }
+
+  minDate: Date; // 最早時間，午夜12時
+  maxDate: any; // 最晚時間，晚間11點59分
+
+  addDispatchWork_form: FormGroup;
+  event_form: FormGroup;
 
   constructor(
     private carServ: CarService,
@@ -42,6 +123,23 @@ export class DispatchComponent implements OnInit {
       updated_at: [''],
       updated_by: [''],
     });
+
+    this.event_form = this.fb.group({
+      id: ['', Validators.required],
+      name: ['', [Validators.required]],
+      start_date: ['', [Validators.required]],
+      end_date: ['', [Validators.required]],
+      type: ['', [Validators.required]],
+      form: [''],
+      driver: [''],
+      description: [''],
+      created_at: [''],
+      updated_at: [''],
+      created_by: [''],
+      updated_by: [''],
+    });
+    // 設定最小日期為今天
+    this.minDate = new Date();
   }
 
   ngOnInit(): void {
@@ -70,12 +168,6 @@ export class DispatchComponent implements OnInit {
         console.log(err);
       },
     });
-  }
-
-  newDispatchVisible = false;
-
-  openNewDispatch() {
-    this.newDispatchVisible = true;
   }
 
 }
