56d94322d30d80b5daa43c297106d3974e5336fb
bovcu13 <jjps30516@gmail.com>
2023-10-27 17:32:27 +0800

    feat: event -> workData

--- src/app/shared/data/event.ts
+++ src/app/shared/data/event.ts
@@ -1,32 +1,47 @@
-export const events = [
+export const workData = [
   {
+    id: 1,
     title: '派工任務 1',
+    form: '運輸訂單02',
     start: '2023-10-05',
     end: '2023-10-05',
+    driver: '李宜蓁',
     allday: true
   },
   {
+    id: 2,
     title: '派工任務 2',
+    form: '運輸訂單02',
     start: '2023-10-10',
     end: '2023-10-10',
+    driver: '李宜蓁',
     allday: true
   },
   {
+    id: 3,
     title: '派工任務 3',
+    form: '運輸訂單02',
     start: '2023-10-15',
     end: '2023-10-15',
+    driver: '李宜蓁',
     allday: true
   },
   {
+    id: 4,
     title: '派工任務 4',
+    form: '運輸訂單02',
     start: '2023-10-20',
     end: '2023-10-20',
+    driver: '李宜蓁',
     allday: true
   },
   {
+    id: 5,
     title: '派工任務 5',
+    form: '運輸訂單02',
     start: '2023-10-25',
     end: '2023-10-25',
+    driver: '李宜蓁',
     allday: true
   }
 ];
