0c5c6704aa37a605a31042086745bc91ddcb4ff4
bovcu13 <jjps30516@gmail.com>
2023-10-27 16:10:14 +0800

    feat: event

---
+++ src/app/shared/data/event.ts
@@ -0,0 +1,33 @@
+export const events = [
+  {
+    title: '派工任務 1',
+    start: '2023-10-05',
+    end: '2023-10-05',
+    allday: true
+  },
+  {
+    title: '派工任務 2',
+    start: '2023-10-10',
+    end: '2023-10-10',
+    allday: true
+  },
+  {
+    title: '派工任務 3',
+    start: '2023-10-15',
+    end: '2023-10-15',
+    allday: true
+  },
+  {
+    title: '派工任務 4',
+    start: '2023-10-20',
+    end: '2023-10-20',
+    allday: true
+  },
+  {
+    title: '派工任務 5',
+    start: '2023-10-25',
+    end: '2023-10-25',
+    allday: true
+  }
+];
+
