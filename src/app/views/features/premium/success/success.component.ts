4a49a53ede6a2aa9801bea5ee0f4e2aa61a3f7df
bovcu13 <jjps30516@gmail.com>
2023-10-30 17:00:36 +0800

    feat: pay-success

--- src/app/views/features/premium/success/success.component.ts
+++ src/app/views/features/premium/success/success.component.ts
@@ -1,10 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router } from "@angular/router";
 
 @Component({
   selector: 'app-success',
   templateUrl: './success.component.html',
-  styleUrls: ['./success.component.css']
+  styleUrls: ['./success.component.scss']
 })
-export class SuccessComponent {
+export class SuccessComponent implements OnInit {
+  countdown: number = 3; // 初始倒计时时间
+  countdownInterval: any;
+
+  constructor(private router: Router) {}
+
+  ngOnInit() {
+    this.startCountdown();
+  }
+
+  startCountdown() {
+    this.countdownInterval = setInterval(() => {
+      this.countdown -= 1;
+      if (this.countdown <= 0) {
+        clearInterval(this.countdownInterval);
+        this.router.navigate(['/main']);
+      }
+    }, 1000); // 更新每1秒
+  }
 
 }
