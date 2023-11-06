8a1c8f13a761c57e116bb631720ffa01acafa911
bovcu13 <jjsp30516@gmail.com>
2023-11-06 10:08:01 +0800

    feat: gps-device delete confirmation message

--- src/app/views/features/fleet-mgmt/gps-device/gps-device.component.ts
+++ src/app/views/features/fleet-mgmt/gps-device/gps-device.component.ts
@@ -1,12 +1,14 @@
-import {Component, OnInit} from '@angular/core';
-import {CarService} from "../../../../services/car.service";
-import {FormBuilder, FormGroup, Validators} from "@angular/forms";
-import {ActivatedRoute, Router} from "@angular/router";
+import { Component, OnInit } from '@angular/core';
+import { CarService } from "../../../../services/car.service";
+import { FormBuilder, FormGroup, Validators } from "@angular/forms";
+import { ActivatedRoute, Router } from "@angular/router";
+import { ConfirmationService, MessageService } from "primeng/api";
 
 @Component({
   selector: 'app-gps-device',
   templateUrl: './gps-device.component.html',
-  styleUrls: ['./gps-device.component.scss']
+  styleUrls: ['./gps-device.component.scss'],
+  providers: [ConfirmationService, MessageService]
 })
 export class GpsDeviceComponent implements OnInit {
   activeIndex = 2;
@@ -17,7 +19,9 @@ export class GpsDeviceComponent implements OnInit {
     private carServ: CarService,
     private fb: FormBuilder,
     private router: Router,
-    private route: ActivatedRoute
+    private route: ActivatedRoute,
+    private messageService: MessageService,
+    private confirmationService: ConfirmationService
   ) {
     this.editGpsDevice_form = this.fb.group({
       firm: ['', Validators.required],
@@ -41,5 +45,42 @@ export class GpsDeviceComponent implements OnInit {
         console.log(err);
       }
     })
+  }
+
+  deleteGpsDeviceRequest() {
+    console.log('open')
+    this.confirmationService.confirm({
+      message: `確定要刪除 ${this.editGpsDevice_form.controls['sid'].value} 嗎？`,
+      header: '確定刪除？',
+      icon: 'pi pi-exclamation-triangle',
+      accept: () => {
+        this.carServ.deleteGpsDeviceRequest(this.id).subscribe({
+          next: res => {
+            console.log('deleteDriver', res);
+            this.showSussess('刪除');
+            this.router.navigate(['/fleet_mgmt',this.activeIndex]);
+          },
+          error: (err) => {
+            console.log('err', err);
+            this.showError('刪除');
+          }
+        })
+      },
+      reject: () => {
+        this.showCancel('刪除');
+      }
+    });
+  }
+
+  showSussess(info = '修改') {
+    this.messageService.add({severity: 'success', summary: '完成', detail: `${info}成功！`});
+  }
+
+  showError(info = '修改') {
+    this.messageService.add({severity: 'error', summary: '錯誤', detail: `${info}失敗！`});
+  }
+
+  showCancel(info: string = '修改') {
+    this.messageService.add({severity: 'warn', summary: '取消', detail: `取消${info}！`});
   }
 }
